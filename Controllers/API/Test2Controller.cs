b3c979f02335e7e1bdf74608f82f5010e41e99e7
ZoeLi <zoeli@tstartel.com>
2022-12-14 17:13:25 +0800

    add new project

---
+++ Controllers/API/Test2Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using test2.Models;
+
+namespace test2.Controllers;
+
+public class Test2Controller : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public Test2Controller(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index2()
+    {
+                // return "???";
+
+        string s = "{\"type\":\"GETBARCODEORDER_2\",\"MSISDN\":\"0908018868\"}";
+
+        return Content(s, "text/json");
+        //return View();
+    }
+
+     public IActionResult Index3()
+    {
+                // return "???";
+
+        return Content("<html><body style='color:red'>test3</body></html>", "text/html");
+        //return View();
+    }
+
+
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using test2.Models;
+
+namespace test2.Controllers;
+
+public class TestController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public TestController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index2()
+    {
+                // return "???";
+
+        return Content("<html><body style='color:red'>test2</body></html>", "text/html");
+        //return View();
+    }
+
+     public IActionResult Index3()
+    {
+                // return "???";
+
+        return Content("<html><body style='color:red'>test3</body></html>", "text/html");
+        //return View();
+    }
+
+
+}
